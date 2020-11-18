3b04412132131d8b0ad9db42cdcd9799954912c4
rakhatsatybaldy <rahat_05_01@mail.ru>
2020-11-18 23:09:58 +0600

    delete one controller

--- src/main/java/jwt/authentication/adminshopmanager/controller/ShopController.java
+++ src/main/java/jwt/authentication/adminshopmanager/controller/ShopController.java
@@ -11,7 +11,6 @@ import jwt.authentication.adminshopmanager.repository.UsersRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
