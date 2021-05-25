00a5f0a5c2017c889af927384bb9661dd277813f
Rakhat <rahat_01_05@mail.ru>
2021-05-25 18:37:57 +0600

    some changes

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,7 @@
 INSERT INTO roles (role_id , role_description , role_name) VALUES (nextval('roles_role_id_seq') , 'DEFAULT ROLE FOR ADMIN' , 'ROLE_ADMIN');
 INSERT INTO users (user_id , user_email , user_password , balance , user_address , blocked , user_full_name , city_city_id ) VALUES (nextval('users_user_id_seq') ,
 'admin' ,
-'$2a$10$nSeRhbMZnpTeueJrxY7sZeYnOkLcLAA.hbWKEmb8TUgrNj2qeqBJS' , 0  , null  , false  , 'Satybaldy Rakhat'  , null );
+'$2y$12$Ux1gIVi8j5vuKebvXzXbeuUcSVIz.ZEV7HQ3dcJ0pHpu8oP4Var3y' , 0  , null  , false  , 'Satybaldy Rakhat'  , null );
 
 
 INSERT INTO users_roles (users_user_id , roles_role_id) VALUES (2 , 2);
